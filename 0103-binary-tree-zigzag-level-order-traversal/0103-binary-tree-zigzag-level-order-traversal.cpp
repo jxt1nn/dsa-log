92a1c05dc5595de170b8a6cc803a5a3004ff3d61
Jatin Kumar <mrsnow693@gmail.com>
2026-04-17 22:26:37 +0530

    Time: 3 ms (8.91%), Space: 15.2 MB (17.37%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,50 @@
+/**
+
+    SIMPLE BFS
+
+    even level insert direct 
+        odd level (reverse then push)
+
+    if not want to reverse insert on it desired index....
+    how?????????
+
+    int index = leftToRight ? i : (size - 1 - i);
+
+
+
+    
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == NULL) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true;  // toggle direction
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level(size);
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                // place node value in correct position
+                int index = leftToRight ? i : (size - 1 - i);
+
+                level[index] = node->val;
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            ans.push_back(level);
+            leftToRight = !leftToRight; // flip direction
+        }
+
+        return ans;
+    }
+};
