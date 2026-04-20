a38e1a9354bba277d2dbe18b44232c8768777650
Jatin Kumar <mrsnow693@gmail.com>
2026-04-20 17:14:43 +0530

    Time: 0 ms (100%), Space: 12 MB (72.82%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -4,22 +4,37 @@
     find fathest differnt number from our number 
     O(n^2)
 
+    but wait.......
 
+    we only care about maximum distance
+    
+    to make it maximum, you want j as far as possible from i
+    The farthest positions in an array are:
+
+    index 0
+    index n-1
+
+    So instead of checking every pair:
+    Compare:
+
+    nums[i] with nums[n-1]
+    nums[i] with nums[0]
 
 */
 class Solution {
 public:
     int maxDistance(vector<int>& nums) {
+        int n = nums.size();
         int maxi = 0;
 
-        for(int i =0;i < nums.size();i++){ 
-            for(int j = nums.size()-1;j >= i;j--){
-                if(nums[i] != nums[j]){
-                    maxi =  max(maxi,abs(i-j));
-                    break;
-                }
-            }
+        for(int i = 0; i < n; i++){
+            if(nums[i] != nums[0])
+                maxi = max(maxi, i);
+
+            if(nums[i] != nums[n-1])
+                maxi = max(maxi, n - 1 - i);
         }
+
         return maxi;
     }
 };
