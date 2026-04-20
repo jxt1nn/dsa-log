f9b8621c5ed7e85a20767082dab27fa5153f4398
Jatin Kumar <mrsnow693@gmail.com>
2026-04-20 16:56:25 +0530

    Time: 0 ms (100%), Space: 12 MB (72.82%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -13,9 +13,10 @@ public:
         int maxi = 0;
 
         for(int i =0;i < nums.size();i++){ 
-            for(int j = i; j < nums.size();j++){
+            for(int j = nums.size()-1;j >= i;j--){
                 if(nums[i] != nums[j]){
                     maxi =  max(maxi,abs(i-j));
+                    break;
                 }
             }
         }
