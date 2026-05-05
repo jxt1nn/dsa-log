8fe8109081f0836737b5e6a2c831f9c8270db768
Jatin Kumar <mrsnow693@gmail.com>
2026-05-05 17:11:19 +0530

    Time: 0 ms (100%), Space: 16.2 MB (94.62%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -12,6 +12,24 @@ and store cut point of second linklist
 
 then join the end of the second list to the head 
 
+
+    break the list → take second part → attach it before head
+
+this is correct for 217/232
+
+but too risky so what we do 
+
+instead of :
+
+    break → then reconnect
+
+Think:
+
+    connect first → then break
+
+        means join the tail to head -> make it circular 
+     then cut the linklist.........
+
  */
 class Solution {
 public:
@@ -42,6 +60,8 @@ public:
         // Step 5: break
         ListNode* newHead = newTail->next;
         newTail->next = NULL;
+
+        // tail->next = head; wrong
 
         return newHead;
     }
