d5302327f8a9327ec3d0af879469678fdf2b38e2
Jatin Kumar <mrsnow693@gmail.com>
2026-05-05 19:56:53 +0530

    Time: 3 ms (27.97%), Space: 10 MB (57.77%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,65 @@
+/*
+all possible subsets : backtrack 
+
+    subset 
+    goal : find all subset 
+    choices : pick/skip
+    constraints : none 
+    base case : reached end
+
+    how to write logic??????????????
+
+    base case :
+    if(i == size()){
+        ans.push_back(temp)
+    }
+
+    loop?????????
+
+    should we need loop???????????
+
+    1.make choice 
+
+    pick : temp.push_back(nums[i])
+    explore : (i+1)
+
+    undo : pop_back
+    explore........
+    
+
+    no we doesnt use loop bcoz of only 2 choices present
+
+
+
+
+*/
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+    
+    void solve(int i,vector<int>& nums){
+        
+        if(i == nums.size()){
+            ans.push_back(temp);
+            return;
+        }  
+
+        //make choice 
+        temp.push_back(nums[i]);
+
+        //explore
+        solve(i+1,nums);
+
+        //undo 
+        temp.pop_back();
+
+        //explore
+        solve(i+1,nums);
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        solve(0,nums);
+        return ans;        
+    }
+};
