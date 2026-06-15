915f248902d536f4acb08acae8500f854ee61368
Jatin Kumar <mrsnow693@gmail.com>
2026-06-15 20:11:54 +0530

    Time: 531 ms (35.78%), Space: 11.8 MB (13.37%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -96,7 +96,10 @@ public:
 
         for(int i = 0;i < n;i++){
             for(int j = 0;j < m;j++){
-
+                
+                if(board[i][j] != word[0])
+                    continue;
+                    
                 vis[i][j] = 1 ;
 
                 if(solve(board,word,0,vis,i,j)){
