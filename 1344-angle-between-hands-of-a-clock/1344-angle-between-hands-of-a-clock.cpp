0369ec704c5dd69fb0357f6eb895c161635ee178
Jatin Kumar <mrsnow693@gmail.com>
2026-06-18 17:04:10 +0530

    Time: 0 ms (100%), Space: 8 MB (95.61%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -1,3 +1,30 @@
+/*
+    1 hour minute angle = 360
+    1 minute angle = 360/60 = 6 
+
+    12 hour angle = 360
+    1 hour angle = 360 / 12 = 30
+
+    hour hand also keep moves with minute hand 
+    
+    so angle per minute in hour is : 30/60 = 0.5
+
+    formula to calculate angle is :
+
+        hourAngle = (hour % 12) * 30 + minutes * 0.5
+
+        minuteAngle = minutes * 6
+
+
+    diff = |hourAngle - minuteAngle|
+
+    Since there are two possible angles between the hands on a circle, return the smaller one:
+
+    min(diff, 360 - diff)
+
+
+
+*/
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
