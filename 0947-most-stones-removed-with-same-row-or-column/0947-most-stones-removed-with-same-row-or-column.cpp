0b0c241f32e9cc3996863eb53a5ad2f51bf2abd9
Jatin Kumar <mrsnow693@gmail.com>
2026-06-27 15:42:31 +0530

    Time: 28 ms (44.57%), Space: 18.2 MB (93.38%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -168,3 +168,36 @@ public:
         return n - components;
     }
 };
+
+/*
+DFS APPROACH 
+
+
+class Solution {
+public:
+    void dfs(int node, vector<bool>& visited, vector<vector<int>>& stones) {
+        visited[node] = 1;
+        for (int j = 0; j < stones.size(); j++) {
+            if (!(visited[j]) &&
+                (stones[j][0] == stones[node][0] || stones[j][1] == stones[node][1])) {
+                dfs(j, visited, stones);
+            }
+        }
+    }
+
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        vector<bool> visited(n, 0);
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(i, visited, stones);
+            }
+        }
+        return n - count;
+    }
+};
+
+*/
