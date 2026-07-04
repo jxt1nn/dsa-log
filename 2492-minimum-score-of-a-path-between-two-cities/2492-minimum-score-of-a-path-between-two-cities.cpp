c789d4540f78842f98b0824345cc5f5adb0544cc
Jatin Kumar <mrsnow693@gmail.com>
2026-07-04 16:43:26 +0530

    Time: 5 ms (99.31%), Space: 107.4 MB (96.89%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -101,8 +101,7 @@ SO, we have to travese the 1's component and find the minimum edge
     Therefore,
     only edges in city 1's connected component are reachable.
 
-
-
+    WE CAN DO THIS BY SIMPLE BFS
 
 */
 class DSU {
@@ -184,3 +183,50 @@ public:
         return ans;
     }
 };
+
+/*
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int,int>>> adj(n + 1);
+
+        for(auto &it : roads){
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        vector<int> vis(n + 1, 0);
+
+        queue<int> q;
+        q.push(1);
+        vis[1] = 1;
+
+        int ans = INT_MAX;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(auto &it : adj[node]){
+                int v = it.first;
+                int dis = it.second;
+
+                ans = min(ans, dis);
+
+                if(!vis[v]){
+                    vis[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+
+
+
+*/
