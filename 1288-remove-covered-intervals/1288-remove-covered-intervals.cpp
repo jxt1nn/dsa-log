e77f63d6e91309d6c7a05d7304dd0d6d849248f2
Jatin Kumar <mrsnow693@gmail.com>
2026-07-06 17:55:01 +0530

    Time: 4 ms (28.48%), Space: 15 MB (82.12%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -13,6 +13,10 @@
             if (j covers i)
                 remove i
 
+    Assume it is not covered.
+    Compare it with every other interval j.
+    If interval j covers interval i, mark i as covered and stop checking further.
+    At the end, count how many intervals were not covered.
 
 
 */
@@ -27,10 +31,14 @@ public:
 
             for (int j = 0; j < n; j++) {
                 if (i == j) continue;
+                int a = intervals[i][0];
+                int b = intervals[i][1];
+                int c = intervals[j][0];
+                int d = intervals[j][1];
 
                 // Does intervals[j] cover intervals[i]?
-                if (intervals[j][0] <= intervals[i][0] &&
-                    intervals[i][1] <= intervals[j][1]) {
+
+                if (c <= a && b <= d) {
                     covered = true;
                     break;
                 }
